7b3f6b12fc105ebb0cf909a9c2e9aeb1f2959341
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-11-29 08:41:41 +0500

    fix client error

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,6 +1,22 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+  
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'), 
+    };
+
+    return config;
+  },
+
+  output: 'standalone', 
 };
 
 export default nextConfig;
