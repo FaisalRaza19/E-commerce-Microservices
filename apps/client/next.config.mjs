50021d0ce6e97595b92d827b23ff89350f219002
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-12-01 17:36:24 +0500

    update files

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,8 +1,14 @@
+import path from "path"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
