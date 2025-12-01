f8a79a462073a534965aa232bf6f1c7b87c3acae
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-12-01 17:40:08 +0500

    done 23

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,10 +1,12 @@
-import path from "path"
+import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "standalone",
+
+  turbopack: false,
+
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
