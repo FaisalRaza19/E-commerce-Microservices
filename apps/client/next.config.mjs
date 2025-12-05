6dd7ac1712d5a133d654f2a7e7cc10047cc2fe4e
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-12-05 14:55:28 +0500

    add debug line

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,19 +1,7 @@
-import path from "path";
-
 const nextConfig = {
   reactCompiler: true,
   output: "standalone",
-
   transpilePackages: ["@repo/eslint-config", "@repo/typescript-config"],
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(process.cwd(), "src"),
-    };
-
-    return config;
-  },
 };
 
 export default nextConfig;
