161bde1c80237fd57cb83830eb8cfb0c2300f4a2
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-12-14 13:17:21 +0500

    done

--- apps/order-service/src/middleware/auth_middleware.js
+++ apps/order-service/src/middleware/auth_middleware.js
@@ -1,5 +1,4 @@
-import pkg from '@clerk/fastify';
-const { getAuth } = pkg;
+import {getAuth} from '@clerk/fastify';
 
 export const userAuth = async (req, res) => {
   const { userId } = getAuth(req);
